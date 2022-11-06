69ef7e6ac2537ca08495859b341c19ae2da7c2ac
Роберт <98408605+jrkmk45@users.noreply.github.com>
2022-11-06 22:36:19 +0300

    added required annotations to create product DTO

--- Shared/Dtos/CreateProductDto.cs
+++ Shared/Dtos/CreateProductDto.cs
@@ -5,8 +5,10 @@ namespace Shared.Dtos
 {
     public class CreateProductDto
     {
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public string Price { get; set; }
         [Required]
         public List<IFormFile> Files { get; set; }
