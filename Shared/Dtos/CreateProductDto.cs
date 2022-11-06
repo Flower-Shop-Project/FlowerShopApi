77094e494dec9fec6cb0086d9020dd000ac25e86
Роберт <98408605+jrkmk45@users.noreply.github.com>
2022-11-06 21:59:32 +0300

    fixed: removed id from CreateProductDto

--- Shared/Dtos/CreateProductDto.cs
+++ Shared/Dtos/CreateProductDto.cs
@@ -5,7 +5,6 @@ namespace Shared.Dtos
 {
     public class CreateProductDto
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Price { get; set; }
